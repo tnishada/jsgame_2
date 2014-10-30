d96a7d93708b2242733ba280d6a74151783cf544
tnishada <lnishada@gmail.com>
2014-10-30 09:30:48 +0530

    objects were divided into seperate javascript files

--- bullet.js
+++ bullet.js
@@ -1,5 +1,5 @@
 /**
- * Created by tharindu on 10/29/2014.
+ * Created by tharindu on 10/30/2014.
  */
 
 /*Bullet Object*/
@@ -22,7 +22,7 @@ function Bullet(){
         ctx.fillStyle = "black";
     };
 
-        /*this method is invoked once ctrl pressed*/
+    /*this method is invoked once ctrl pressed*/
     this.shoot = function(angle){
 
         this.locationx = shooter.locationx + (shooterRadius+gun_length)*Math.cos(Math.PI*2*angle/360);
@@ -45,45 +45,5 @@ function Bullet(){
             this.bulletEnabled = false;
         }
     }
-
-}
-
-
-
-/*stone object*/
-function Stone(x,y,radius,angle){
-    this.locationx=x;
-    this.locationy = y;
-    this.angle = angle;
-    this.stoneEnabled = true;
-    this.radius =radius;
-    this.speed = 1;
-    this.dx = 3;
-    this.dy = 3;
-
-    this.dx = this.speed*Math.cos(Math.PI*2*angle/360);
-    this.dy = -this.speed*Math.sin(Math.PI*2*angle/360);
-
-    this.draw = function(){
-        ctx.fillStyle = "green";
-        ctx.beginPath();
-        ctx.arc(this.locationx, this.locationy,this.radius, 0, Math.PI*2, true); // x-position, y-position,arc_radius, start angle , end angle, clockwise
-        ctx.closePath();
-        ctx.fill();
-        ctx.fillStyle = "black";
-    };
-
-    this.handle = function(){
-
-        if(this.locationx-radius <= WIDTH &&this.locationx+radius>=0 && this.locationy-radius<=HEIGHT && this.locationy+radius>=0 && this.stoneEnabled){
-
-            this.draw(this.locationx,this.locationy);
-            this.locationx += this.dx;
-            this.locationy +=this.dy;
-
-        }else{   //stone is out of the canvas thus reset everything
-            this.stoneEnabled = false;
-        }
-    };
 
 }
